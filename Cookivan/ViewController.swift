f469c5f713d022407a5e103dcf6c91231cb292fb
Angelina F <angelina.friz@gmail.com>
2019-07-14 10:42:28 +0300

    Initial Commit

---
+++ Cookivan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cookivan
+//
+//  Created by Angelina on 7/14/19.
+//  Copyright © 2019 Angelina Friz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
