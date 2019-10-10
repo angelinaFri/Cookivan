3c2730012df784c7f62dfaac06d5bc78cfb63316
Angelina F <angelina.friz@gmail.com>
2019-10-10 08:02:26 +0300

    Screenshots for READ.me added

--- Cookivan/Controller/RecipeVC.swift
+++ Cookivan/Controller/RecipeVC.swift
@@ -12,25 +12,18 @@ class RecipeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navBarSetUp()
 
+    }
+
+    func navBarSetUp() {
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
         self.navigationController?.navigationBar.isTranslucent = true
         self.navigationController?.view.backgroundColor = .clear
-
-
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
