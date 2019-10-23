653e49794103034264e0f6f3964a322617250cf6
Andrius Shiaulis <shiaulis@gmail.com>
2019-10-23 23:28:41 +0300

    Remove unused method from PlainTableViewCell class

--- Cookivan/Xibs/PlainTableViewCell.swift
+++ Cookivan/Xibs/PlainTableViewCell.swift
@@ -18,9 +18,4 @@ class PlainTableViewCell: UITableViewCell {
         cellImage.applyshadowWithCorner(containerView: containerView, cornerRadious: 10)
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 }
